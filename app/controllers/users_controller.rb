3b8b70f131767c5b4aa40020707b068ceb7c14f0
YasmineOsama <yasmine.osama68@gmail.com>
2018-06-19 12:57:53 +0200

    Fixed minor bug in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :firstName, :lastName)
+    params.require(:user).permit(:email, :password, :name)
   end
 
   def set_user
