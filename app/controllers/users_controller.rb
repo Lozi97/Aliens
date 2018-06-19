9c4f022e3ae2514fd36f90aa869a1c7604f31cf5
YasmineOsama <yasmine.osama68@gmail.com>
2018-06-19 12:55:13 +0200

    Users_controller and it's routes added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+  skip_before_action :authenticate_request
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+
+  def show
+    json_response(@user)
+  end
+
+  def create
+    @user = User.create(user_params)
+    json_response(@user)
+  end
+
+  def update
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :firstName, :lastName)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   resources :alliens
-  
-  post 'authenticate', to: 'authentication#authenticate'
+
+  resources :users
+
+  post '/login', to: 'authentication#authenticate'
+
+  post '/signup', to: "users#create"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
