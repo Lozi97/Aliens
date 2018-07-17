18ca82187cd17f6633ec87b239ba7a16aa181af7
YasmineOsama <yasmine.osama68@gmail.com>
2018-07-17 12:15:30 +0200

    Added pagination

--- app/controllers/api/v1/admin/alliens_controller.rb
+++ app/controllers/api/v1/admin/alliens_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Admin::AlliensController < Api::V1::Admin::AdminController
     before_action :set_allien, only: [:show, :update, :destroy]
 
   def index
-    @alliens = Allien.paginate(:page => params[:page])
+    @alliens = Allien.paginate(:page => params[:page], :per_page => 4)
     json_response(@alliens)
   end
 
@@ -29,7 +29,7 @@ class Api::V1::Admin::AlliensController < Api::V1::Admin::AdminController
   private
 
   def allien_params
-    params.permit(:name, :image, :desc, :per_page)
+    params.permit(:name, :image, :desc)
   end
 
   def set_allien
